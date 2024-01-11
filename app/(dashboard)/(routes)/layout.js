c2211b00e6a3bb477e1acd89837ca1067a33a88c
Chidi <chidiamaraekejiuba@gmail.com>
2024-01-11 04:59:28 +0000

    working on side navbar

--- app/(dashboard)/(routes)/layout.js
+++ app/(dashboard)/(routes)/layout.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import SideNavBar from '../_components/SideNavBar'
 
 const layout = ({children}) => {
   return (
       <div>
-        <h2>Home</h2>
-        {children}
+        <div className='h-full w-64 flex-col fixed inset-y-0 z-50'>
+          <SideNavBar/>
+        </div>
+        <div className='ml-64'>
+          {children}
+        </div>
     </div>
   )
 }
